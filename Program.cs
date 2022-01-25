e36510cd0c07d736f55a30e29064f8dad70cfc5d
franciscoamaral0 <franciscogurgel@me.com>
2022-01-25 17:28:10 +0000

    test c-sharp

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var texto = "ola testando";
+      var texto2 = "ola testando";
+
+      Console.WriteLine(texto.Length);
+      Console.WriteLine(texto.Equals(texto2));
+      Console.WriteLine(texto.IndexOf("ndo"));
+      Console.WriteLine(texto.Replace("ola", "olár"));
+    }
+  }
+}
